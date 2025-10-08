dd7e5d34edf93f63332c68fc1f1751f7d6b280f1
i519181 - Lukas Buncak <l.buncak@fhict.nl>
2025-10-08 02:08:13 +0200

    project creation

---
+++ ticketingapp/src/main/java/nl/fontys/s7/ticketingapp/TicketingappApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.s7.ticketingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingappApplication.class, args);
+	}
+
+}
---
+++ ticketingapp/src/test/java/nl/fontys/s7/ticketingapp/TicketingappApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.s7.ticketingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
